c7a2d48639e1f2556b3cf273d101b3e5a3a55fa8
Jodi Kraushar <jodi.kraushar@kellogg.northwestern.edu>
2024-03-10 17:26:51 +0000

    hw4 WIP

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,12 +4,18 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["user_id"] = @current_user["id"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
